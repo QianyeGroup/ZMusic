5d7b90270783d9449b85e02e7be5e95bc25f0fc6
真心 <1307993674@qq.com>
2021-09-24 20:07:46 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/RealHeart/ZMusic")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
